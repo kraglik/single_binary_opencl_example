07f7b27d6c0606a5f7c20d0d27a77b15b0538442
kraglik <igor.kraglik@yandex.ru>
2021-01-22 20:33:12 +0500

    style update

--- cmake/preprocess_kernel.cmake
+++ cmake/preprocess_kernel.cmake
@@ -1,12 +1,12 @@
 function(preprocess_opencl_kernel NAMESPACE KERNEL SRC DST)
     message(STATUS "Preprocessing OpenCL kernel: " ${SRC} " => " ${DST} )
     add_custom_command(
-            OUTPUT ${DST}
-            COMMAND ${CMAKE_COMMAND}
-            -DDST=${DST} -DSRC=${SRC}
-            -DNAMESPACE=${NAMESPACE} -DKERNEL=${KERNEL}
-            -P "${CMAKE_SOURCE_DIR}/cmake/scripts/preprocess_kernel.cmake"
-            MAIN_DEPENDENCY ${SRC}
+        OUTPUT ${DST}
+        COMMAND ${CMAKE_COMMAND}
+        -DDST=${DST} -DSRC=${SRC}
+        -DNAMESPACE=${NAMESPACE} -DKERNEL=${KERNEL}
+        -P "${CMAKE_SOURCE_DIR}/cmake/scripts/preprocess_kernel.cmake"
+        MAIN_DEPENDENCY ${SRC}
     )
 endfunction(preprocess_opencl_kernel)
 
@@ -22,7 +22,8 @@ function(preprocess_opencl_kernels)
         string(SUBSTRING ${kernel_filename} 0 ${kernel_filename_length} kernel_name)
 
         preprocess_opencl_kernel(${namespace} ${kernel_name}
-                ${kernel}
-                ${out_dir}/${kernel_name}_kernel.cpp)
+            ${kernel}
+            ${out_dir}/${kernel_name}_kernel.cpp
+        )
     endforeach(kernel)
 endfunction(preprocess_opencl_kernels)
